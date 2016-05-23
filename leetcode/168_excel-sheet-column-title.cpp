0805ab0b362b6785b9b8c7f8ea59f747f54c89a7
Almer Bolatov <almer.bolatov@gmail.com>
2016-05-23 08:18:39 +0600

    Leetcode solutions

--- leetcode/168_excel-sheet-column-title.cpp
+++ leetcode/168_excel-sheet-column-title.cpp
@@ -1,8 +1,12 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
   public:
     string convertToTitle(int n) {
-        if (n < 1)
-            return "";
+        if (n < 1) return "";
 
         string s = "";
         int mx = 26;
@@ -17,3 +21,10 @@ class Solution {
         return s;
     }
 };
+
+int main() {
+    int n = 79;
+    Solution s;
+    cout << s.convertToTitle(n) << endl;
+    return 0;
+}
