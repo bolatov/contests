c60a2ea2f94b48c1185a68f1547717449709c44c
Almer Bolatov <almer.bolatov@gmail.com>
2016-12-02 15:52:05 +0600

    Menshikov course hw4, 630A

---
+++ codeforces.ru/p630a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    int n;
+    cin >> n;
+    puts("25");
+    return 0;
+}
